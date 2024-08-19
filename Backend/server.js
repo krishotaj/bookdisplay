ba1563ae476ec8e5e10435d717b51224dba6e84e
krishotaj <kristianahotaj99@gmail.com>
2024-08-19 22:05:37 +0300

    backend completed

--- Backend/server.js
+++ Backend/server.js
@@ -3,27 +3,18 @@ require("dotenv").config();
 const connection = require("./Config/Connection");
 const app = express();
 const port = 8888;
-const routes = require('./Routes/Routes');
+const routes = require("./Routes/Routes");
 const cors = require("cors");
+
+
+
 app.use(express.json());
-app.use(cors(
-    { origin: "*" })
-);
-
-app.get('/users', async (req, res) => {
-    try {
-      const users = await User.find(); 
-      res.json(users);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  });
-
-
-  app.use('/api', routes);
+app.use(cors({ origin: "*" }));
+app.use(express.urlencoded({extended: true,}));
+app.use("/api", routes);
 
 
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
-    });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
